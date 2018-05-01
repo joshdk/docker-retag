84115416ea6a7ff343b68ab71fa6902cd9d47aa5
Josh Komoroske <jdkomo@gmail.com>
2018-04-30 21:18:58 -0700

    Parsing of command line arguments

--- main.go
+++ main.go
@@ -5,8 +5,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -17,5 +19,40 @@ func main() {
 }
 
 func mainCmd(args []string) error {
+	var (
+		repository, oldTag, newTag, err = parseArgs(args)
+	)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(repository, oldTag, newTag)
+
 	return nil
+}
+
+func parseArgs(args []string) (string, string, string, error) {
+	switch len(args) {
+	case 4:
+		// given:  "docker-retag", "repo/product", "1.2.3", "4.5.6"
+		// return: "repo/product", "1.2.3", "4.5.6", nil
+		return args[1], args[2], args[3], nil
+
+	case 3:
+		chunks := strings.SplitN(args[1], ":", 2)
+		if len(chunks) == 2 {
+
+			// given:  "docker-retag", "repo/product:1.2.3", "4.5.6"
+			// return: "repo/product", "1.2.3", "4.5.6", nil
+			return chunks[0], chunks[1], args[2], nil
+		}
+
+		// given:  "docker-retag", "repo/product", "4.5.6"
+		// return: "repo/product", "latest", "4.5.6", nil
+		return chunks[0], "latest", args[2], nil
+
+	default:
+		return "", "", "", errors.New("invalid arguments")
+	}
 }
